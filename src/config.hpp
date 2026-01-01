b47cf21e8a998dd241217920b70536132b869f68
Henrik <CatEars13@gmail.com>
2026-01-01 14:29:02 +0000

    Extend with file operations value

--- src/config.hpp
+++ src/config.hpp
@@ -31,7 +31,8 @@ enum class config_parse_error
     parse_error,
     not_an_object,
     missing_field,
-    field_wrong_type
+    field_wrong_type,
+    field_unexpected_value
 };
 
 std::ostream &operator<<(std::ostream &os, const config_parse_error &op)
@@ -46,6 +47,8 @@ std::ostream &operator<<(std::ostream &os, const config_parse_error &op)
         return os << "missing_field";
     case config_parse_error::field_wrong_type:
         return os << "field_wrong_type";
+    case config_parse_error::field_unexpected_value:
+        return os << "field_unexpected_value";
     default:
         return os << "unknown";
     }
@@ -57,51 +60,114 @@ struct file_node_from_config
     std::vector<file_node_from_config> children;
     file_ops file_ops = file_ops::passthrough;
 
-    bool is_directory() const {
+    bool is_directory() const
+    {
         return this->children.size() == 0;
     }
 };
 
-leaf::result<file_node_from_config> _recursive_parse(const boost::json::value& val) 
+leaf::result<void> _parse_name(const boost::json::object &config_object, file_node_from_config &ret)
 {
-    file_node_from_config ret;
-    auto obj = val.if_object();
-    if (obj == nullptr) {
-        return leaf::new_error(config_parse_error::not_an_object);
-    }
-    auto config_object = *obj;
     auto name_ptr = config_object.if_contains("name");
-    if (name_ptr == nullptr) {
+    if (name_ptr == nullptr)
+    {
         return leaf::new_error(config_parse_error::missing_field);
     }
     auto name = name_ptr->if_string();
-    if (name == nullptr) {
+    if (name == nullptr)
+    {
         return leaf::new_error(config_parse_error::field_wrong_type);
     }
     ret.name = *name;
+    return {};
+}
+
+leaf::result<void> _parse_operation(const boost::json::object &config_object, file_node_from_config &ret)
+{
+    auto ops_ptr = config_object.if_contains("operation");
+    if (ops_ptr == nullptr)
+    {
+        return {};
+    }
+
+    auto ops = *ops_ptr;
+    auto cast_ops = ops.if_string();
+    if (cast_ops == nullptr)
+    {
+        return leaf::new_error(config_parse_error::field_wrong_type);
+    }
+
+    auto ops_str = *cast_ops;
+    if (ops_str == "passthrough")
+    {
+        ret.file_ops = file_ops::passthrough;
+    }
+    else if (ops_str == "log")
+    {
+        ret.file_ops = file_ops::log;
+    }
+    else if (ops_str == "fail")
+    {
+        ret.file_ops = file_ops::fail;
+    }
+    else if (ops_str == "slow")
+    {
+        ret.file_ops = file_ops::slow;
+    }
+    else
+    {
+        return leaf::new_error(config_parse_error::field_unexpected_value);
+    }
+
+    return {};
+}
+
+leaf::result<file_node_from_config> _recursive_parse(const boost::json::value &val);
+
+leaf::result<void> _parse_children(const boost::json::object &config_object, file_node_from_config &ret)
+{
     auto children_ptr = config_object.if_contains("children");
-    if (children_ptr != nullptr) {
+    if (children_ptr != nullptr)
+    {
         auto cast_children_ptr = children_ptr->if_array();
-        if (cast_children_ptr == nullptr) {
+        if (cast_children_ptr == nullptr)
+        {
             return leaf::new_error(config_parse_error::field_wrong_type);
         }
         auto children = *cast_children_ptr;
-        for (auto child : children) {
+        for (auto child : children)
+        {
             BOOST_LEAF_AUTO(child_result, _recursive_parse(child));
             ret.children.push_back(child_result);
         }
     }
+    return {};
+}
+
+leaf::result<file_node_from_config> _recursive_parse(const boost::json::value &val)
+{
+    file_node_from_config ret;
+    auto obj = val.if_object();
+    if (obj == nullptr)
+    {
+        return leaf::new_error(config_parse_error::not_an_object);
+    }
+    auto config_object = *obj;
+    BOOST_LEAF_CHECK(_parse_name(config_object, ret));
+    BOOST_LEAF_CHECK(_parse_operation(config_object, ret));
+    BOOST_LEAF_CHECK(_parse_children(config_object, ret));
     return ret;
 }
 
-leaf::result<file_node_from_config> read_configuration_stream(std::istream& io)
+leaf::result<file_node_from_config> read_configuration_stream(std::istream &io)
 {
     boost::json::parse_options parse_opts;
     parse_opts.allow_comments = true;
     parse_opts.allow_trailing_commas = true;
     std::error_code ec;
     auto val = boost::json::parse(io, ec, {}, parse_opts);
-    if (ec) {
+    if (ec)
+    {
         return leaf::new_error(config_parse_error::parse_error);
     }
     return _recursive_parse(val);
--- src/test_config.cxx
+++ src/test_config.cxx
@@ -17,45 +17,53 @@
  */
 #define BOOST_TEST_MODULE Read configuration
 #include <boost/test/included/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
 #include <sstream>
 #include "config.hpp"
 #include "plannable_operations.hpp"
 
-file_node_from_config read_valid_config_string(const std::string& config) {
+file_node_from_config read_valid_config_string(const std::string &config)
+{
     std::stringstream ss(config);
     auto result = read_configuration_stream(ss);
     BOOST_TEST(!result.has_error());
     return result.value();
 }
 
-leaf::result<file_node_from_config> read_invalid_config_string(const std::string& config) {
+leaf::result<file_node_from_config> read_invalid_config_string(const std::string &config)
+{
     std::stringstream ss(config);
     auto result = read_configuration_stream(ss);
     BOOST_TEST(result.has_error());
     return result;
 }
 
-void assert_error_code(const std::string& config, config_parse_error expected_err) {
+void assert_error_code(const std::string &config, config_parse_error expected_err)
+{
     auto _ = leaf::try_handle_some(
-        [&](){ return read_invalid_config_string(config); },
-        [&](const config_parse_error& err, const leaf::error_info& ei) -> leaf::result<file_node_from_config> {
+        [&]()
+        { return read_invalid_config_string(config); },
+        [&](const config_parse_error &err, const leaf::error_info &ei) -> leaf::result<file_node_from_config>
+        {
             BOOST_TEST(err == expected_err);
             return ei.error();
         },
-        [](const leaf::error_info& ei) -> leaf::result<file_node_from_config> {
+        [](const leaf::error_info &ei) -> leaf::result<file_node_from_config>
+        {
             BOOST_FAIL("Did not get expected error code");
             return ei.error();
-        }
-    );
+        });
 }
 
-BOOST_AUTO_TEST_CASE(can_read_with_a_root_config) {
+BOOST_AUTO_TEST_CASE(can_read_with_a_root_config)
+{
     auto config = read_valid_config_string(R"({"name":""})");
     BOOST_TEST(config.children.size() == 0);
     BOOST_TEST(config.name == "");
 }
 
-BOOST_AUTO_TEST_CASE(can_specify_child_nodes) {
+BOOST_AUTO_TEST_CASE(can_specify_child_nodes)
+{
     auto config = read_valid_config_string(R"({
         "name":"", 
         "children": [
@@ -81,23 +89,45 @@ BOOST_AUTO_TEST_CASE(can_specify_child_nodes) {
     BOOST_TEST(config.children[1].children[0].children.size() == 0);
 }
 
-BOOST_AUTO_TEST_CASE(default_file_ops_is_passthrough) {
+BOOST_AUTO_TEST_CASE(default_file_ops_is_passthrough)
+{
     auto config = read_valid_config_string(R"({"name":""})");
     BOOST_TEST(config.file_ops == file_ops::passthrough);
 }
 
-BOOST_AUTO_TEST_CASE(returns_error_when_name_is_wrong_type) {
+BOOST_AUTO_TEST_CASE(returns_error_when_name_is_wrong_type)
+{
     assert_error_code(R"({"name":true})", config_parse_error::field_wrong_type);
 }
 
-BOOST_AUTO_TEST_CASE(returns_error_when_children_is_wrong_type) {
+BOOST_AUTO_TEST_CASE(returns_error_when_children_is_wrong_type)
+{
     assert_error_code(R"({"name":"","children":false})", config_parse_error::field_wrong_type);
 }
 
-BOOST_AUTO_TEST_CASE(returns_error_when_name_is_not_there) {
+BOOST_AUTO_TEST_CASE(returns_error_when_name_is_not_there)
+{
     assert_error_code(R"({"not-name":""})", config_parse_error::missing_field);
 }
 
-BOOST_AUTO_TEST_CASE(returns_error_when_invalid_json) {
+BOOST_AUTO_TEST_CASE(returns_error_when_invalid_json)
+{
     assert_error_code(R"({"name":"")", config_parse_error::parse_error);
+}
+
+std::vector<file_ops> tested_file_ops{
+    file_ops::passthrough,
+    file_ops::fail,
+    file_ops::log,
+    file_ops::slow};
+
+namespace bdata = boost::unit_test::data;
+
+BOOST_DATA_TEST_CASE(can_parse_all_valid_file_enums, bdata::make(tested_file_ops), file_op)
+{
+    std::stringstream ss;
+    ss << file_op;
+    auto config = R"({"name":"","operation":")" + ss.str() + R"("})";
+    auto result = read_valid_config_string(config);
+    BOOST_TEST(result.file_ops == file_op);
 }
